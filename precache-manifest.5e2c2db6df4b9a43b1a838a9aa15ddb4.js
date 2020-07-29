239c08a3d5f961cb3c8458c2ad89f9b927ddc4c3
Bonggeun Cho <bonggeuncho@Bonggeunui-MacBook-Pro.local>
2020-07-29 13:56:03 +0900

    Updates

---
+++ precache-manifest.5e2c2db6df4b9a43b1a838a9aa15ddb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5305f2c6228e1453c888b7225264c715",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "0cef8124a5960830f2ff",
+    "url": "/movie_app_2019/static/css/main.6fe6c1ef.chunk.css"
+  },
+  {
+    "revision": "fde87d2ad6cabe52f608",
+    "url": "/movie_app_2019/static/js/2.c3d73534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.c3d73534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cef8124a5960830f2ff",
+    "url": "/movie_app_2019/static/js/main.780169fc.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
